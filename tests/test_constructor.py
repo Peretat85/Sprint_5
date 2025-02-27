7eb6dfaf93d7ddedcab80e17933dea511a2180c2
Peretat <peretat@mail.ru>
2025-02-27 12:43:32 +0300

    27/02/2025 исправления test_constructor.py и test_entrance.py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,15 +1,12 @@
-import select
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from data import Data
-#from helper import Helper
 from locators import Locators
 
-# переход В ЛК для авторизованного пользователя
+# тест перехода в раздел Начинок
 class TestConstructor:
-    def test_constructor(self, driver):
+    def test_constructor_fillings(self, driver):
         # Переход на страницу авторизации (Входа)
         driver.get(Data.STELLAR)
 
@@ -18,7 +15,15 @@ class TestConstructor:
         enter_button.click()
 
         # ожидаем перехода на раздел Начинок
-        WebDriverWait(driver, Data.WAIT_TIME).until(EC.text_to_be_present_in_element(Locators.STELLAR_FILLINGS_SECTION,'Начинки'))
+        WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.STELLAR_FILLINGS_SECTION))
+
+        fillings_view = driver.find_element(*Locators.STELLAR_FILLINGS_SECTION)
+        assert fillings_view.is_displayed()
+
+# тест перехода в раздел Соусы
+    def test_constructor_sous(self,driver):
+        # Переход на страницу авторизации (Входа)
+        driver.get(Data.STELLAR)
 
         # нажатие на кнопку СОУСЫ
         enter_button = driver.find_element(*Locators.STELLAR_SOUS)
@@ -27,9 +32,27 @@ class TestConstructor:
         # ожидаем перехода на раздел Соусов
         WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.STELLAR_SOUS_SECTION))
 
+        sous_view = driver.find_element(*Locators.STELLAR_SOUS_SECTION)
+        assert sous_view.is_displayed()
+
+# тест перехода в раздел Булки
+    def test_constructor_buns(self,driver):
+        # Переход на страницу авторизации (Входа)
+        driver.get(Data.STELLAR)
+
+        # скроллинг вниз, чтобы скрыть раздел Булки
+        # нажатие на кнопку Начинки
+        enter_button = driver.find_element(*Locators.STELLAR_FILLINGS)
+        enter_button.click()
+        # ожидаем перехода на раздел Начинок
+        WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.STELLAR_FILLINGS_SECTION))
+
         # нажатие на кнопку Булки
         enter_button = driver.find_element(*Locators.STELLAR_BUNS)
         enter_button.click()
 
         # ожидаем перехода на раздел Булки
         WebDriverWait(driver, Data.WAIT_TIME).until(EC.visibility_of_element_located(Locators.STELLAR_BUNS_SECTION))
+
+        buns_view = driver.find_element(*Locators.STELLAR_BUNS_SECTION)
+        assert buns_view.is_displayed()
--- tests/test_entrance.py
+++ tests/test_entrance.py
@@ -1,10 +1,7 @@
-from selenium.webdriver.common.by import By
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from data import Data
-#from helper import Helper
 from locators import Locators
 
 class TestEntranse:
